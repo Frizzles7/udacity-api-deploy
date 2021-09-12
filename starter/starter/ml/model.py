e337f7c60b20e0147e75b558d15859fbd9e71853
Megan McGee <mtorau@gmail.com>
2021-09-11 20:07:22 -0500

    add training code

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,36 +1,39 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import GradientBoostingClassifier
 
 
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
-    Trains a machine learning model and returns it.
+    Trains a machine learning model and returns it
 
     Inputs
     ------
     X_train : np.array
-        Training data.
+        Training data
     y_train : np.array
-        Labels.
+        Labels
     Returns
     -------
     model
         Trained machine learning model.
     """
 
-    pass
+    model = GradientBoostingClassifier(subsample=0.8, max_features=0.8,
+    	random_state=1).fit(X_train, y_train)
+    
+    return model
 
 
 def compute_model_metrics(y, preds):
     """
-    Validates the trained machine learning model using precision, recall, and F1.
+    Validates the trained machine learning model using precision, recall, and F1
 
     Inputs
     ------
     y : np.array
-        Known labels, binarized.
+        Known labels, binarized
     preds : np.array
-        Predicted labels, binarized.
+        Predicted labels, binarized
     Returns
     -------
     precision : float
@@ -44,17 +47,20 @@ def compute_model_metrics(y, preds):
 
 
 def inference(model, X):
-    """ Run model inferences and return the predictions.
+    """ Run model inferences and return the predictions
 
     Inputs
     ------
-    model : ???
-        Trained machine learning model.
+    model :
+        Trained machine learning model
     X : np.array
-        Data used for prediction.
+        Data used for prediction
     Returns
     -------
     preds : np.array
-        Predictions from the model.
+        Predictions from the model
     """
-    pass
+    preds = model.predict(X)
+    
+    return preds
+
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,12 +1,18 @@
-# Script to train machine learning model.
+# Script to train machine learning model
+
 
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+# Add the necessary imports for the starter code
+from ml.data import process_data
+from ml.model import train_model
+import pandas as pd
+import joblib
 
-# Add code to load in the data.
+# Add code to load in the data
+data = pd.read_csv("../data/census_clean.csv")
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Perform train-test split
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,10 +25,21 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+# Process the training data
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data
+X_text, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, 
+    encoder=encoder, lb=lb
+)
 
-# Train and save a model.
+# Train and save the model, encoder, and lb
+model = train_model(X_train, y_train)
+joblib.dump(model, "model.pkl")
+joblib.dump(encoder, "encoder.pkl")
+joblib.dump(lb, "lb.pkl")
+
